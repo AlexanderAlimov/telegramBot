e8c52cd1ab6e07f7e8f6f8557c974bbe2b2ef75b
AlexanderAlimov <sashaalimov5@gmail.com>
2021-04-13 12:40:03 +0300

    remove comments

--- src/server/servieces/pgBoss.js
+++ src/server/servieces/pgBoss.js
@@ -9,28 +9,6 @@ const createReminderWithPgBoss = async () => {
   
   }
   
-//   async function someAsyncJobHandler(job) {
-//     console.log("job", job);
-//     console.log(`job ${job.id} received with data:`);
-//     console.log(JSON.stringify(job.data));
-  
-//     await doSomethingAsyncWithThis(job.data);
-//   }
-
-//   async function doSomethingAsyncWithThis(data){
-//     const TelegramBot = require("node-telegram-bot-api");
-//     const Bot = require("../models").Bots;
-//     const myBot = await Bot.findOne({where:{id: data.botId}});
-//     const bot = new TelegramBot(myBot.token, { polling: true });
-//     bot.sendMessage(data.chatId, "Check pg boss!");
-//     bot.stopPolling();
-//   }
-
   module.exports.pgBossInitialize = createReminderWithPgBoss;
   module.exports.pgBoss = boss;
 
-
-
-
-
-// await boss.subscribe(queue, someAsyncJobHandler);
